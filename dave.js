7ce4219e0442df08d248f0c7f230c7a2329d948a
Dave Snowberger <git@fartherout.org>
2017-12-22 07:08:01 -0800

    fixed typo

--- dave.js
+++ dave.js
@@ -6,9 +6,9 @@ function Note() {
   //the top-left corner's xy-coordinates of the format [x,y]
   this.coords = [400,400];
 
-  //dimensions of the note
-  this.height = 400px;
-  this.width = 400px;
+  //dimensions of the note in pixels
+  this.height = 400;
+  this.width = 400;
  
   //the text contents of the note:
   this.contents = '';
