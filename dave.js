d6f8f6e437572df550a5aab1930ecee444494060
Dave Snowberger <git@fartherout.org>
2017-12-23 12:57:31 -0800

    added event handling inside input field to trigger save

--- dave.js
+++ dave.js
@@ -64,6 +64,7 @@ function Note() {
     //event listeners
     noteHeaderEl.addEventListener('mousedown', this.startMove.bind(this));
     noteResizeEl.addEventListener('mousedown', this.startResize.bind(this));
+    noteInputEl.addEventListener('keyup', this.save.bind(this));
     window.addEventListener('mouseup', this.stopInterval.bind(this));
 
     //build note element and attach to DOM
