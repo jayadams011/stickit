f1ac236846e6366b1afe940fad9f978c5a528ebb
IFREECAN <jasper.kinnay@gmmail.com>
2017-12-26 15:03:52 -0800

    createFuction2Render2page

--- jasper.js
+++ jasper.js
@@ -1,28 +1,17 @@
-
+'use strict';
 
 //create a variable for button element
 var newNoteButton = document.getElementById('newNoteButton');
 
 //create an event listener that listens for click-event on newNoteButton to render a new-note to page.
 //listen for click --> addNote
- newNoteButton.addEventListener('click', addNote);
-
+newNoteButton.addEventListener('click', addNote);
 
 
 //create function to render newNote to page
-addNote = function(){
-
-  if (button. == "noterender")
-  //create new note object.
-
-  //render that object to the screen when addEventListener hears the click
-  
-
-  render new note... exicute "noterender"
-   else {
-      //do not render note i.e. unrender the render function
-     this.unrender = function() {
-       var noteEl = document.getElementById(this.id);
-       noteEl.parentNode.removeChild(noteEl);
-   }
+function addNote(){
+  //create a new instance of "Note"...
+  var note = new Note(); //
+  //render 'note' to screen.
+  note.render(); //i.e - render the actual 'note' to the sceen on the idea 'Note'..
 };
