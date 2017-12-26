2686614f5500a979cb604fc5bd9db3e451d6df20
IFREECAN <jasper.kinnay@gmmail.com>
2017-12-26 14:31:13 -0800

    add eventlister to variable

---
+++ jasper.js
@@ -0,0 +1,28 @@
+
+
+//create a variable for button element
+var newNoteButton = document.getElementById('newNoteButton');
+
+//create an event listener that listens for click-event on newNoteButton to render a new-note to page.
+//listen for click --> addNote
+ newNoteButton.addEventListener('click', addNote);
+
+
+
+//create function to render newNote to page
+addNote = function(){
+
+  if (button. == "noterender")
+  //create new note object.
+
+  //render that object to the screen when addEventListener hears the click
+  
+
+  render new note... exicute "noterender"
+   else {
+      //do not render note i.e. unrender the render function
+     this.unrender = function() {
+       var noteEl = document.getElementById(this.id);
+       noteEl.parentNode.removeChild(noteEl);
+   }
+};
